101dbf0faef5d53c8594888d29a8687e2cac0dac
Dele Olajide <dele@4ng.net>
2020-03-02 10:15:29 +0000

    added PDF creation to search plugin

--- search/search.js
+++ search/search.js
@@ -27,7 +27,7 @@
                          '<div class="modal-body">' +
                          '<input id="pade-search-keywords" class="form-control" type="text" placeholder="Type a query and press [Enter] to search" ><p/><div id="pade-search-results"></div>' +
                          '</div>' +
-                         '<div class="modal-footer"> <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button> </div>' +
+                         '<div class="modal-footer"> <button type="button" class="btn btn-success btn-pdf">PDF</button><button type="button" class="btn btn-danger" data-dismiss="modal">Close</button> </div>' +
                          '</div> </div> </div>';
                 },
                 afterRender() {
@@ -46,6 +46,7 @@
                   }, false);
                 },
                 events: {
+                    'click .btn-pdf': 'doPDF',
                     'keyup #pade-search-keywords': 'keyUp'
                 },
 
@@ -57,7 +58,29 @@
                         this.doSearch();
                     }
                 },
+                doPDF() {
+                    const margins = {
+                      top: 70,
+                      bottom: 40,
+                      left: 30,
+                      width: 550
+                    };
+                    const pdf = new jsPDF('p','pt','a4');
 
+                    pdf.autoTable({
+                        head: [['Date', 'Person', 'Message']],
+                        body: this.model.get("pdf_body"),
+                        columnStyles: {
+                            0: {cellWidth: 100},
+                            1: {cellWidth: 100},
+                            2: {cellWidth: 300}
+                        }
+                    })
+
+                    const view = this.model.get("view");
+                    const roomLabel = view.model.getDisplayName() || view.model.get("jid");
+                    pdf.save(roomLabel + '.pdf')
+                },
                 doSearch() {
                     var view = this.model.get("view");
                     var jid = view.model.get("jid");
@@ -67,59 +90,66 @@
 
                     var that = this;
                     var keyword = that.model.get("keyword");
-                    var searchRegExp = new RegExp('^(.*)(\s?' + keyword + ')', 'i');
-                    var tagRegExp = new RegExp("(\\b" + keyword + "\\b)", "im");
+                    var searchRegExp = undefined;
+                    var tagRegExp = undefined;
+                    var pdf_body = [];
 
                     console.debug("doSearch", keyword, jid);
 
                     if (keyword != "")
                     {
-                        var searchResults = that.el.querySelector("#pade-search-results");
-                        searchResults.innerHTML = "No Match";
-                        var html = "<table style='margin-left: 15px'><tr><th>Date</th><th>Person</th><th>Message</th></tr>";
+                        searchRegExp = new RegExp('^(.*)(\s?' + keyword + ')', 'i');
+                        tagRegExp = new RegExp("(\\b" + keyword + "\\b)", "im");
+                    }
+                    var searchResults = that.el.querySelector("#pade-search-results");
+                    searchResults.innerHTML = "No Match";
+                    var html = "<table style='margin-left: 15px'><tr><th>Date</th><th>Person</th><th>Message</th></tr>";
 
-                        if (method == "mam")
+                    if (method == "mam")
+                    {
+                        _converse.api.archive.query({before: '', max: 999, 'groupchat': groupchat, 'with': jid}).then(function(result)
                         {
-                            _converse.api.archive.query({before: '', max: 999, 'groupchat': groupchat, 'with': jid}).then(function(result)
-                            {
-                                const messages = result.messages;
-
-                                for (var i=0; i<messages.length; i++)
-                                {
-                                    if (messages[i].querySelector('body'))
-                                    {
-                                        var body = messages[i].querySelector('body').innerHTML;
-                                        var delay = messages[i].querySelector('forwarded').querySelector('delay');
-                                        var from = messages[i].querySelector('forwarded').querySelector('message').getAttribute('from');
-                                        var time = delay ? delay.getAttribute('stamp') : dayjs().format();
-                                        var pretty_time = dayjs(time).format('MMM DD<br/>HH:mm:ss');
-                                        var pretty_from = type === "chatroom" ? from.split("/")[1] : from.split("@")[0];
-
-                                        html =  html + makeHtml(searchRegExp, tagRegExp, body, pretty_time, pretty_from);
-                                    }
-                                }
-
-                                html =  html + "</table>";
-                                searchResults.innerHTML = html;
-                            });
-                        }
-                        else {
-                            var messages = view.model.messages.models;
+                            const messages = result.messages;
 
                             for (var i=0; i<messages.length; i++)
                             {
-                                var body = messages[i].get('message');
-                                var from = messages[i].get('from');
-                                var pretty_time = dayjs(messages[i].get('time')).format('MMM DD<br/>HH:mm:ss');
-                                var pretty_from = from;
-                                if (from) pretty_from =  messages[i].get('type') === "groupchat" ? from.split("/")[1] : from.split("@")[0];
+                                if (messages[i].querySelector('body'))
+                                {
+                                    var body = messages[i].querySelector('body').innerHTML;
+                                    var delay = messages[i].querySelector('forwarded').querySelector('delay');
+                                    var from = messages[i].querySelector('forwarded').querySelector('message').getAttribute('from');
+                                    var time = delay ? delay.getAttribute('stamp') : dayjs().format();
+                                    var pretty_time = dayjs(time).format('MMM DD HH:mm:ss');
+                                    var pretty_from = type === "chatroom" ? from.split("/")[1] : from.split("@")[0];
+                                    if (!searchRegExp || searchRegExp.test(body)) pdf_body.push([pretty_time, pretty_from, body]);
 
-                                html =  html + makeHtml(searchRegExp, tagRegExp, body, pretty_time, pretty_from);
+                                    html =  html + makeHtml(searchRegExp, tagRegExp, body, pretty_time, pretty_from);
+                                }
                             }
 
                             html =  html + "</table>";
                             searchResults.innerHTML = html;
+                            that.model.set("pdf_body", pdf_body);
+                        });
+                    }
+                    else {
+                        var messages = view.model.messages.models;
+
+                        for (var i=0; i<messages.length; i++)
+                        {
+                            var body = messages[i].get('message');
+                            var from = messages[i].get('from');
+                            var pretty_time = dayjs(messages[i].get('time')).format('MMM DD HH:mm:ss');
+                            var pretty_from = from;
+                            if (from) pretty_from =  messages[i].get('type') === "groupchat" ? from.split("/")[1] : from.split("@")[0];
+                            if (!searchRegExp || searchRegExp.test(body)) pdf_body.push([pretty_time, pretty_from, body]);
+
+                            html =  html + makeHtml(searchRegExp, tagRegExp, body, pretty_time, pretty_from);
                         }
+
+                        html =  html + "</table>";
+                        searchResults.innerHTML = html;
+                        that.model.set("pdf_body", pdf_body);
                     }
                 }
             });
@@ -171,9 +201,9 @@
     {
         let html = "";
 
-        if (searchRegExp.test(body))
+        if (!searchRegExp || searchRegExp.test(body))
         {
-            var tagged = body.replace(tagRegExp, "<span style=background-color:#FF9;color:#555;>$1</span>");
+            var tagged = tagRegExp ? body.replace(tagRegExp, "<span style=background-color:#FF9;color:#555;>$1</span>") : body;
             html = html + "<tr><td>" + pretty_time + "</td><td>" + pretty_from + "</td><td>" + tagged + "</td></tr>";
         }
         return html;
