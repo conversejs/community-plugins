b5828bacbd72c0536dbca5f838dab18085030541
Dele Olajide <dele@4ng.net>
2020-10-21 23:36:43 +0100

    add diagram plugin

---
+++ packages/diagrams/diagrams.js
@@ -0,0 +1,64 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(["converse"], factory);
+    } else {
+        factory(converse);
+    }
+}(this, function (converse) {
+    var _converse = null, html;
+
+    converse.plugins.add("diagrams", {
+        'dependencies': [],
+
+        'initialize': function () {
+            _converse = this._converse;
+            html = converse.env.html;
+
+            _converse.api.listen.on('afterMessageBodyTransformed', function(model, text)
+            {
+               console.debug("afterMessageBodyTransformed", model, text);
+               doDiagram(model.get("body"), text, model.get("msgid"));
+            });
+
+            mermaid.initialize({});
+
+            console.debug("diagrams plugin is ready");
+        }
+    });
+
+    function doDiagram(body, text, msgId)
+    {
+        console.debug("doDiagram", body, text, msgId);
+
+        if (body.startsWith("graph TD") ||
+            body.startsWith("graph TB") ||
+            body.startsWith("graph BT") ||
+            body.startsWith("graph RL") ||
+            body.startsWith("graph LR") ||
+            body.startsWith("pie") ||
+            body.startsWith("gantt") ||
+            body.startsWith("stateDiagram") ||
+            body.startsWith("classDiagram") ||
+            body.startsWith("sequenceDiagram") ||
+            body.startsWith("erDiagram")) {
+
+            text.addTemplateResult(0, body.length, html`<br/><div id="mermaid-${msgId}" class="mermaid">\n${body.replace(/<br>/g, '\n')}\n</div>`);
+
+            setTimeout(function()
+            {
+                window.mermaid.init(document.querySelector("#mermaid-" + msgId));
+            }, 500);
+        }
+        else
+
+        if (body.startsWith("X:1"))
+        {
+            text.addTemplateResult(0, body.length, html`<div id="abc-${msgId}"></div>`);
+
+            setTimeout(function()
+            {
+                ABCJS.renderAbc("abc-" + msgId, body.replace(/<br>/g, '\n'));
+            }, 500);
+        }
+    }
+}));
